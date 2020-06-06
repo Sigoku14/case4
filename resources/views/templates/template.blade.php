b7ed837bc0deaad823f631e98c889b15eb5c7d39
HikaruKitayama <hikaru.ktym@gmail.com>
2020-06-06 19:00:28 +0900

    HOME作成

---
+++ resources/views/templates/template.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    @yield('header')
+</head>
+
+<body>
+    <header>
+        <h1>感染リスク感知マン</h1>
+    </header>
+    <main id="wrapper">
+        @yield('content')
+    </main>
+    <footer>
+        <small>© 2020 HAL. All Rights Reserved.</small>
+    </footer>
+</body>
+
+</html>
