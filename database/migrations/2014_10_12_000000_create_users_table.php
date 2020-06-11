3adf6555b1c6c25bed92cd9bc09cb5dff4ba89d3
hiraken011011 <aaa1192aaa2960@gmail.com>
2020-06-11 18:22:41 +0900

    マイグレーションの追加
    users、locations、tweets、infections

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            // $table->id();
+            $table->bigIncrements('id')->comment('ユーザーID');
+            $table->string('password')->comment('ユーザーパスワード');
+            $table->integer('status')->comment('状態'); // ->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
