a4539cf98b1fb54df311e9c43b592fc3d4cfa09c
HikaruKitayama <hikaru.ktym@gmail.com>
2020-06-12 12:03:25 +0900

    マイグレーション一部削除

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,32 +42,32 @@ class UserController extends Controller
             return view("sign/signIn");
         }
     }
-    public function regist(Request $request)
-    {
-        $mail = $request->input('mail');
-        $password = $request->input('password');
+    // public function regist(Request $request)
+    // {
+    //     $mail = $request->input('mail');
+    //     $password = $request->input('password');
 
-        $validationMsgs = [];
-        if (isset($mail) && isset($password)) {
-            $us_ma = DB::table('users')
-                ->where('mail', $mail)
-                ->first();
+    //     $validationMsgs = [];
+    //     if (isset($mail) && isset($password)) {
+    //         $us_ma = DB::table('users')
+    //             ->where('mail', $mail)
+    //             ->first();
 
-            if ($us_ma !== null) {
-                $validationMsgs['mail'] = "そのメールアドレスは既に登録されています。";
-                return view("sign/signUp")->with('msg', $validationMsgs);
-            } else {
-                $created_at = date('Y-m-d H:i:s');
-                $updated_at = date('Y-m-d H:i:s');
-                $status = 1;
-                DB::table('users')->insert(
-                    ['mail' => $mail, 'password' => $password, 'created_at' => $created_at, 'updated_at' => $updated_at, 'status' => $status]
-                );
-                $id = DB::getPdo()->lastInsertId();
-                return redirect()->action('HomeController@home', ['id' => $id]);
-            }
-        } else {
-            return view("sign/signUp");
-        }
-    }
+    //         if ($us_ma !== null) {
+    //             $validationMsgs['mail'] = "そのメールアドレスは既に登録されています。";
+    //             return view("sign/signUp")->with('msg', $validationMsgs);
+    //         } else {
+    //             $created_at = date('Y-m-d H:i:s');
+    //             $updated_at = date('Y-m-d H:i:s');
+    //             $status = 1;
+    //             DB::table('users')->insert(
+    //                 ['mail' => $mail, 'password' => $password, 'created_at' => $created_at, 'updated_at' => $updated_at, 'status' => $status]
+    //             );
+    //             $id = DB::getPdo()->lastInsertId();
+    //             return redirect()->action('HomeController@home', ['id' => $id]);
+    //         }
+    //     } else {
+    //         return view("sign/signUp");
+    //     }
+    // }
 }
