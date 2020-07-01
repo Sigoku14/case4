741b0d90f2a89b987d286d7c54fe6fe27f0ba984
HikaruKitayama <hikaru.ktym@gmail.com>
2020-07-01 12:21:05 +0900

    Twitterのcsv化

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -11,11 +11,53 @@ class TwitterController extends Controller
     public function index(Request $request)
     {
         //ツイートを5件取得
-        $result = \Twitter::get('statuses/home_timeline');
+        $result = \Twitter::get('statuses/home_timeline', array("q" => "コロナ", 'count' => 20));
 
         //ViewのTwitter.blade.phpに渡す
         return view('twitter', [
             "result" => $result
         ]);
+    }
+
+    public function makeCsv(Request $request)
+    {
+        // リスト
+        $lists = [
+            ['おはよう', 'おやすみ'],
+            ['こんにちは', 'さようなら'],
+        ];
+
+        $filename = 'demo.csv';
+        $csv_file_path = storage_path('app/' . $filename);
+        $result = fopen($csv_file_path, 'w');
+        if ($result === false) {
+            throw new Exception('ファイルの書き込みに失敗しました。');
+        } else {
+            fwrite($result, pack('C*', 0xEF, 0xBB, 0xBF)); // BOM をつける
+        }
+        fclose($result);
+        $file = $result;
+
+        // ヘッダー
+        $array = ['header1', 'header2'];
+        $result = fopen($file, 'a');
+        // ファイルに書き出し
+        fputcsv($result, $array);
+        fclose($result);
+
+
+        // 値を入れる
+        foreach ($lists as $list) {
+            $result = fopen($file, 'a');
+            // ファイルに書き出し
+            fputcsv($result, $list);
+            fclose($result);
+        }
+
+        $response = file_get_contents($file);
+
+        return response($response, 200)
+            ->header('Content-Type', 'text/csv')
+            ->header('Content-Disposition', 'attachment; filename=' . $filename);
     }
 }
--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/case4/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/case4/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/case4/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/case4/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
